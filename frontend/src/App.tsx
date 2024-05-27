506ca030ef0ecd7ed1b843c00090eed79836f396
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-27 17:28:32 +0200

    Solved challenge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {response} from "./characters.ts";
+import {ChangeEvent, useState} from "react";
+
+type origin = {
+    name: string
+    url: string
+}
+
+type location = {
+    name: string
+    url: string
+}
+
+type Character = {
+    id: number,
+    name: string,
+    status: string,
+    species: string,
+    type: string,
+    gender: string,
+    origin: origin,
+    location: location,
+    image: string,
+    episode: string[],
+    url: string,
+    created: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const characters: Character[] = response.results;
+    const [filteredCharacters, setFilteredCharacters] = useState<Character[]>(response.results);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const filterCharacters = (event: ChangeEvent<HTMLInputElement>) => {
+        setFilteredCharacters(characters.filter(character => character.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    };
+
+    return (
+        <>
+            <input onChange={filterCharacters}/>
+            {filteredCharacters.map((character) => (
+                <p key={character.id}>{character.name}</p>
+            ))}
+            {filteredCharacters.length === 0 && <p>Sorry, I couldn't find your character...</p>}
+        </>
+    )
 }
 
 export default App
